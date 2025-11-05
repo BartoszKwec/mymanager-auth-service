071632d9bc146e4b3d96c14d09b5dc9f68397bae
BartoszKwec <bartosz.kwec@softwarehut.com>
2025-11-05 11:41:28 +0100

    Initial Spring Boot project setup

---
+++ src/main/java/pl/kwec/authservice/MymanagerAuthServiceApplication.java
@@ -0,0 +1,13 @@
+package pl.kwec.authservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MymanagerAuthServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MymanagerAuthServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/kwec/authservice/MymanagerAuthServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.kwec.authservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MymanagerAuthServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
